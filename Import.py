caac258eb76fee242461640877282723c3b93d94
dan771 <49430372+dan771@users.noreply.github.com>
2022-04-07 06:30:37 +0100

    Update Import.py
    Updated to correct discord token

--- Import.py
+++ Import.py
@@ -1,6 +1,6 @@
 import os
 
-os.system('cmd /c "dotnet DicordChatExporter.CLI\\DiscordChatExporter.Cli.dll export -t "OTYxMTU1NDIzODU0MDA2MzYz.Yk14ww.3jIrCl1O_e3VWPKbuHn4SWxsLAk" -c 939905115505180682 -o Chat.txt -f PlainText"')
+os.system('cmd /c "dotnet DicordChatExporter.CLI\\DiscordChatExporter.Cli.dll export -t "OTYxMTU1NDIzODU0MDA2MzYz.Yk32PA.lOtwgA3mv6KEhtpGZAUcTyce0O8" -c 939905115505180682 -o Chat.txt -f PlainText"')
 #Gets a list of all messages from maps-pinboard
 
 with open('Chat.txt', 'r', encoding="utf8") as f:
